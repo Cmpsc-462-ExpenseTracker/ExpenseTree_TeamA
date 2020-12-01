5c09b7741d910ccf473a0749a5446e548c5e637f
Matt C <55063086+MCoutts@users.noreply.github.com>
2020-12-01 16:09:17 -0500

    Update expenseTracker.py
    Classifier function was missing.

--- expenseTracker_integrateGUI_test/expenseTracker.py
+++ expenseTracker_integrateGUI_test/expenseTracker.py
@@ -258,16 +258,107 @@ class expenseTree:
             return
         self.checkForBalanace(currNode.parent)
 
+def classifier(item):  # Himani: commenting outline
+    # store off cost and name from item for classification use
+    cost = item.cost
+    name = item.name
+    category = "" #type of category
+    levelOfNeed = "necessity" #necessity (default) or luxury
+
+    # create a list of keywords for each type of personal expense:
+    #perhaps the GUI can have the user select one of these words when entering their expense
+    housing = ["housing", "mortgage", "rent", "property tax", "repairs", "housing fees"]
+    transportation = ["transportation", "car mortgage", "bus", "train", "gas", "tires", "repairs", "parking fees", "maintenance",
+                      "warranty", "uber", "taxi"]
+    food = ["food", "groceries", "restaurants", "pet food", "snacks"]
+    utilities = ["utilities","electricity", "water", "garbage", "sewage", "heating", "cooling", "AC", "mobile network", "internet",
+                 "cable", "laundry"]
+    clothing = ["clothing","professional attire", "formal wear", "casual wear", "shoes", "accessories"]
+    healthcare = ["healthcare", "medicine","primary care", "dental care", "specialty care", "urgent care", "medication", "medical devices",
+                  "support", "nursing"]
+    insurance = ["insurance","health insurance", "homeowner's insurance", "car insurance", "life insurance", "disability insurance"]
+    householdItems = ["household items", "toiletries", "laundry detergent", "dishwasher detergent", "cleaning supplies", "tools"]
+    personal = ["personal", "memberships", "haircut", "salon", "cosmetics", "babysitter", "birthday", "anniversary", "holiday",
+                "wedding"]
+    loans = ["personal loan", "student loan", "credit card"]
+    education = ["education","textbooks", "student fees", "lab fees", "school supplies", "clubs", "conferences"]
+    savings = ["savings", "emergency", "long-term savings"]
+    entertainment = ["entertainment", "games", "movie", "concerts", "party", "vacations", "alcohol", "subscription", "sport",
+                    "social events"]
+
+#check if the item name fits in one of these categories
+    if (name in housing):
+        category = "housing"
+    elif(name in transportation):
+        category = "transportation"
+    elif(name in food):
+        category = "food"
+    elif(name in utilities):
+        category = "utilities"
+    elif(name in clothing):
+        category = "clothing"
+    elif(name in healthcare):
+        category = "healthcare"
+    elif(name in insurance):
+        category = "insurance"
+    elif(name in householdItems):
+        category = "household items"
+    elif(name in personal):
+        category = personal
+    elif(name in loans):
+        category = "loans"
+    elif(name in education):
+        category = "education"
+    elif(name in savings):
+        category = "savings"
+    elif(name in entertainment):
+        category = "entertainment"
+    else:
+        category = "miscellaneous" #if category isn't identified
+
+#luxury if personal, entertainment (may need adjusting - perhaps add functions to tweak definition of "luxury" for each user using input)
+    if (category == "personal" or category == "entertainment"):
+        levelOfNeed = "luxury"
+
+#returns an array of the category and sub category:
+    return [category, levelOfNeed]
 
 
+item = itemNode("movie", 20)
+categoryList = classifier(item)
+item.setCategory(categoryList[0])
+item.setLevelOfNeed(categoryList[1])
+item.toString()
+
+item2 = itemNode("water", 70)
+categoryList = classifier(item2)
+item2.setCategory(categoryList[0])
+item2.setLevelOfNeed(categoryList[1])
+item2.toString()
+
+item3 = itemNode("professional attire", 100)
+categoryList = classifier(item3)
+item3.setCategory(categoryList[0])
+item3.setLevelOfNeed(categoryList[1])
+item3.setDescription("new suit for interview")
+item3.toString()
 '''
-class Classifier: #TODO Himani: commenting outline
-    def __init__(self,listnode)
-        self.neccesity
-        self.luxury
-    # luxury
-    #
-    pass'''
+**SAMPLE CLASSIFIER RUN: ***
+name: movie
+cost: 20
+category: entertainment
+level of need: luxury
+description: none
+name: water
+cost: 70
+category: utilities
+level of need: necessity
+description: none
+name: professional attire
+cost: 100
+category: clothing
+level of need: necessity
+description: new suit for interview
 
 """monthlyList = monthlyExpenses()
 Lunch = itemNode("Salad", cost=100)
